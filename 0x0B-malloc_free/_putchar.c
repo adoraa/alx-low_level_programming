76e1166823c596aeb6eb333ea3102ed0d5720e41
Adora <adoraonyile@gmail.com>
2023-11-20 22:51:33 +0100

    redo

--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _putchar - writes character to stdout
+ * _putchar - writes characters to stdout
  * @c: character to be printed to stdout
  * Return: returns 1 on success, -1 on error and
  * sets errno
