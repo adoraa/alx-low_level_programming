167f42eaed6ef55ee6604a0f737e7e5f2a790219
Adora <adoraonyile@gmail.com>
2023-11-30 23:01:43 +0100

    prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list nList;
+	unsigned int i = 0, count = 0;
+	char *s;
+
+	va_start(nList, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		if (count)
+			printf(", ");
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(nList, char));
+				break;
+			case 'i':
+				printf("%i", va_arg(nList, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(nList, float));
+				break;
+			case 's':
+				s = va_arg(nList, char *);
+				if (s != NULL)
+				{
+					printf("%s", s);
+					break;
+				}
+				printf("nil");
+				break;
+			default:
+				count = 0;
+				i++;
+				continue;
+		}
+		count = 1;
+		i++;
+	}
+	printf("\n");
+	va_end(nList);
+}
