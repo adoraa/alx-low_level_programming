0ac5c46c1a8c4b9812fcfba3dc90310935f7b489
Adora <adoraonyile@gmail.com>
2023-12-07 20:02:58 +0100

    prints a string

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_text -  prints String
+ * Return: void
+ */
+void __attribute__((constructor)) print_text(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
