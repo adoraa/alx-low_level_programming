e8f50475ba3b9403115708af6e230d88a6ae8e04
Adora <adoraonyile@gmail.com>
2023-12-15 05:13:43 +0100

    added primary support files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary digit to be converted
+ * Return: the converted number, or 0 if there is one
+ * or more chars in the string b that is not 0 or 1.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+}
