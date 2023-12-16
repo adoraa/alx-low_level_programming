95e449dd4b23520d133775f00377e0239a31c477
Adora <adoraonyile@gmail.com>
2023-12-16 01:07:27 +0100

    debugging

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int val;
 
 	if (!index)
-		return (0);
+		return (n & 1);
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	val = n >> index;
