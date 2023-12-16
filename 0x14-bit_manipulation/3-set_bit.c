1fbffc66dfa636f06355e2dee88578d4bc35a177
Adora <adoraonyile@gmail.com>
2023-12-16 03:20:53 +0100

    bug fix

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,7 +10,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int val = 1;
 
 	val <<= index;
-	if (!index)
+	if (index == 0)
 		return ((*n) & 1);
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
