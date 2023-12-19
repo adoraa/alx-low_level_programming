e1a431f1bbcda0c2e450d11021b2c65eda15c570
Adora <adoraonyile@gmail.com>
2023-12-19 06:26:09 +0100

    appends text at the end of a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t writeBytes;
+
+	if (!filename | !text_content)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	writeBytes = write(fd, text_content, _strlen(text_content));
+	if (writeBytes == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
