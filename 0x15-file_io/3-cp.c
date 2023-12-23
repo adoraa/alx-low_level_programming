3d2edf2afb4b914e9f3e1d5e09ea8df4ece83c72
Adora <adoraonyile@gmail.com>
2023-12-23 05:34:19 +0100

    checker error fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,24 +21,21 @@ void error_handler(int cmd, const char *msg)
 int main(int argc, char *argv[])
 {
 	char *file_from, *file_to;
-	int fd_from, fd_to;
 	char buffer[BUFFER_SIZE];
 	ssize_t readBytes, writeBytes;
 
 	if (argc != 3)
-	{
 		error_handler(97, "Usage: cp file_from file_to");
-	}
 	file_from = argv[1];
 	file_to = argv[2];
-	fd_from = open(file_from, O_RDONLY);
+	int fd_from = open(file_from, O_RDONLY);
 
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fd_to == -1)
 	{
@@ -48,6 +45,7 @@ int main(int argc, char *argv[])
 	while ((readBytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		writeBytes = write(fd_to, buffer, readBytes);
+
 		if (writeBytes != readBytes)
 			error_handler(99, "Error: Write failed");
 	}
@@ -59,8 +57,6 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 	if (close(fd_to) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-	}
 	return (0);
 }
